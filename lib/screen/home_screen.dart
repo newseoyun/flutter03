42195a28361842c09accc991050dc2e8df0dfc54
macSeoyun <newseoyun@gmail.com>
2021-10-09 22:59:32 +0900

    .

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter03/model/model_movie.dart';
+import 'package:flutter03/widget/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
@@ -21,7 +22,18 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            CarouselImage(
+              movies: movies,
+            ),
+            TopBar(),
+          ],
+        )
+      ],
+    );
   }
 }
 
