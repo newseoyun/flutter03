634c5e4d004687f3d9ee2c8c2c621e3116a0aff5
macSeoyun <newseoyun@gmail.com>
2021-10-18 21:32:15 +0900

    add profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter03/screen/home_screen.dart';
+import 'package:flutter03/screen/more_screen.dart';
 import 'package:flutter03/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +28,7 @@ class _MyAppState extends State<MyApp> {
                 HomeScreen(),
                 Container(),
                 Container(),
-                Container(),
+                MoreScreen(),
               ],
             ),
             bottomNavigationBar: Bottom(),
--- lib/screen/more_screen.dart
+++ lib/screen/more_screen.dart
@@ -3,6 +3,54 @@ import 'package:flutter/material.dart';
 class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 50),
+              child: const CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage('images/bbongflix_logo.png'),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 15),
+              child: const Text(
+                'Hihi~',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.white),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              width: 140,
+              height: 5,
+              color: Colors.red,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text('hihihihihihihi'),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: FlatButton(
+                onPressed: () {},
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.edit),
+                    Text('프로필 수정하기'),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
